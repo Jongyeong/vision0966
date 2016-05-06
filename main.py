a438e3c50cff45ddbdf0ffc7bb442096864b7a3b
Jongyeong <hotapero1080@gmail.com>
2016-05-06 17:01:31 +0900

    Added files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import argparse
+import base64
+import httplib2
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+# The url template to retrieve the discovery document for trusted testers.
+DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+
+
+def main(photo_file):
+    """Run a label request on a single image"""
+
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('vision', 'v1', credentials=credentials,
+                              discoveryServiceUrl=DISCOVERY_URL)
+
+    with open(photo_file, 'rb') as image:
+        image_content = base64.b64encode(image.read())
+        service_request = service.images().annotate(body={
+            'requests': [{
+                'image': {
+                    'content': image_content.decode('UTF-8')
+                },
+                'features': [{
+                    'type': 'LOGO_DETECTION',
+                    'maxResults': 10
+                }]
+            }]
+        })
+        response = service_request.execute()
+        for i 0:10      
+            logo = response['responses'][0]['logoAnnotations'][i]['description']
+            if logo != null
+                print('Found Logo: %s for %s' % (logo, photo_file))
+            else continue       
+        return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_file', help='The image you\'d like to label.')
+    args = parser.parse_args()
+    main(args.image_file)
+    
