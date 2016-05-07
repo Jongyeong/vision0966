78a06b58b50cc983850999b5a9ffae9d4eb3f2d7
JongYeong Lee <hotapero1080@gmail.com>
2016-05-07 02:07:17 +0000

    API key 연결로 변경

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}
 def main(photo_file):
     """Run a label request on a single image"""
 
-    credentials = GoogleCredentials.get_application_default()
-    service = discovery.build('vision', 'v1', credentials=credentials,
-                              discoveryServiceUrl=DISCOVERY_URL)
+    service = discovery.build('vision', 'v1', developerKey='AIzaSyBRfze9MmgvJsYaaa99dUSbathSpJFvQCI')
 
     with open(photo_file, 'rb') as image:
         image_content = base64.b64encode(image.read())
