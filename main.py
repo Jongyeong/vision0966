169e4ac4765c5e08b1a7c9bb38f429c3dc42dc4a
source_server <source_server@example.com>
2016-05-13 09:40:50 +0000

    modify workspace

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import base64
 import json
 import sys
 
-from ㅣㅑㅠ.googleapiclient import discovery
+from lib.googleapiclient import discovery
 import httplib2
 from oauth2client.client import GoogleCredentials
 
