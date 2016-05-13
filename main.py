06a6ea7ab1bef281bd8766b498c38dc0d4aa4bb3
JongYeong Lee <hotapero1080@gmail.com>
2016-05-13 03:09:37 +0000

    구글클라우드 api를 그대로 복사하여 실행환경 확인

--- main.py
+++ main.py
@@ -1,43 +1,88 @@
-import argparse
-import base64
-import httplib2
+#!/usr/bin/env python
 
-import discovery
+# Copyright 2016 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Identifies the landmark for the given image."""
+
+import argparse
+
+from googleapiclient import discovery
+import httplib2
 from oauth2client.client import GoogleCredentials
 
+
+# [START get_vision_service]
 # The url template to retrieve the discovery document for trusted testers.
 DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
 
 
-def main(photo_file):
-    """Run a label request on a single image"""
+def get_vision_service():
+    credentials = GoogleCredentials.get_application_default()
+    return discovery.build('vision', 'v1', credentials=credentials,
+                           discoveryServiceUrl=DISCOVERY_URL)
+# [END get_vision_service]
 
-    service = discovery.build('vision', 'v1', developerKey='AIzaSyBRfze9MmgvJsYaaa99dUSbathSpJFvQCI')
 
-    with open(photo_file, 'rb') as image:
-        image_content = base64.b64encode(image.read())
-        service_request = service.images().annotate(body={
-            'requests': [{
-                'image': {
-                    'content': image_content.decode('UTF-8')
-                },
-                'features': [{
-                    'type': 'LOGO_DETECTION',
-                    'maxResults': 10
-                }]
+# [START identify_landmark]
+def identify_landmark(gcs_uri, max_results=4):
+    """Uses the Vision API to identify the landmark in the given image.
+    Args:
+        gcs_uri: A uri of the form: gs://bucket/object
+    Returns:
+        An array of dicts with information about the landmarks in the picture.
+    """
+    batch_request = [{
+        'image': {
+            'source': {
+                'gcs_image_uri': gcs_uri
+            }
+        },
+        'features': [{
+            'type': 'LANDMARK_DETECTION',
+            'maxResults': max_results,
             }]
+        }]
+
+    service = get_vision_service()
+    request = service.images().annotate(body={
+        'requests': batch_request,
         })
-        response = service_request.execute()
-        for i in range(10) :      
-            logo = response['responses'][0]['logoAnnotations'][i]['description']
-            if logo != null :
-                print('Found Logo: %s for %s' % (logo, photo_file))
-            else : continue       
-        return 0
+    response = request.execute()
+
+    return response['responses'][0].get('landmarkAnnotations', None)
+# [END identify_landmark]
+
+
+# [START main]
+def main(gcs_uri):
+    if gcs_uri[:5] != 'gs://':
+        raise Exception('Image uri must be of the form gs://bucket/object')
+    annotations = identify_landmark(gcs_uri)
+    if not annotations:
+        print('No landmark identified')
+    else:
+        print('\n'.join(a['description'] for a in annotations))
+# [END main]
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('image_file', help='The image you\'d like to label.')
+    parser = argparse.ArgumentParser(
+        description='Identifies the landmark in the given image.')
+    parser.add_argument(
+        'gcs_uri', help=('The Google Cloud Storage uri to the image to identify'
+                         ', of the form: gs://bucket_name/object_name.jpg'))
     args = parser.parse_args()
-    main(args.image_file)
-    
+
+    main(args.gcs_uri)
